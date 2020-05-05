05f4e3927489dc0443ae7a0ac648b26393deb64d
Julien Rhodes <julien.rhodes@planet.com>
2020-05-05 11:54:56 -0700

    working with master

--- src/main/scala/mylib/MyTopLevelSim.scala
+++ src/main/scala/mylib/MyTopLevelSim.scala
@@ -37,15 +37,20 @@ object MyTopLevelSim {
 
       def tdi_shift(data : Int, size : Int) : Int = {
         var dataOut : Int = 0
-        for( i <- 0 to (size - 1) ) {
+        for( i <- 0 to (size - 2) ) {
           dut.io.jtag.tdi #= ((data >>> i) & 1) == 1
           // the last digit is shifted during TMS transition
-          if (i < size - 1) {
-            dut.ctrl.clockDomain.waitSampling()
-          }
+          dut.ctrl.clockDomain.waitSampling()
           if (dut.io.jtag.tdo.toBoolean) {
             dataOut |= 1 << i
           }
+        }
+        // Exit SHIFT
+        dut.io.jtag.tms #= true
+        dut.ctrl.clockDomain.waitSampling()
+        // Collect the last bit
+        if (dut.io.jtag.tdo.toBoolean) {
+          dataOut |= 1 << (size - 1)
         }
         return dataOut
       }
@@ -53,24 +58,25 @@ object MyTopLevelSim {
 
       // Switch to shift IR
       //tms_shift(List(true, true, false))
-      tms_shift("110")
+      tms_shift("1100")
       
       // Shift 4 into IR
       var shiftOut = tdi_shift(4, 8)
-      //assert(shiftOut == 0x1, f"Unexpected IR: $shiftOut%X")
+      assert(shiftOut == 0x4, f"Unexpected IR: $shiftOut%X")
       //tdi_shift(List(false, false, true, false, false, false, false))
 
-      // Update IR, return to IDLE
-      tms_shift("110")
+      // Exit IR -> Update IR -> IDLE
+      tms_shift("10")
 
       // Switch to shift DR
       tms_shift("100")
 
       // Read TDO out of DR
-      shiftOut = tdi_shift(0, 33)
+      shiftOut = tdi_shift(0, 32)
       assert(shiftOut == 0x87654321, f"Unexpected ID: $shiftOut%X")
 
-      dut.ctrl.clockDomain.waitSampling()
+      // Exit DR -> Update DR -> IDLE
+      tms_shift("10")
 
     }
   }
